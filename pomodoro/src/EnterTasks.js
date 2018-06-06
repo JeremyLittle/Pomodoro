fa4ac85da4f3a53fb7c6f672fed301ba48e751be
Sonali Luthar <sonaliluthar@d-172-25-79-163.dhcp.virginia.edu>
2018-06-06 13:16:15 -0400

    create component for entering completed tasks

---
+++ pomodoro/src/EnterTasks.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import {
+  Checkbox,
+  Radio,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+  Button
+} from "react-bootstrap";
+import firebase from "./firebase.js";
+
+export default class EnterTasks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      tasks: [],
+      enter: [],
+      isClicked: false
+    };
+  }
+
+  componentDidMount() {}
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleClick = e => {
+    if (this.state.value !== null) {
+      let newTask = this.state.tasks;
+      newTask.push(this.state.value);
+      this.setState({ tasks: newTask });
+    }
+    this.setState({ isClicked: true });
+  };
+
+  setisClickedFalse = () => {
+    this.setState({ isClicked: false });
+  };
+
+  addEnterLine = () => {
+    let newEnter = this.state.enter;
+    newEnter.push(
+      <form>
+        <FormGroup controlId="formBasicText">
+          <FormControl
+            type="text"
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <Button bsStyle="primary" bsSize="xsmall" onClick={this.handleClick}>
+            Add another task
+          </Button>
+        </FormGroup>
+      </form>
+    );
+    this.setState({ enter: newEnter });
+  };
+
+  render() {
+    console.log(this.state.tasks);
+    if (this.state.isClicked) {
+      this.addEnterLine();
+      this.setisClickedFalse();
+    }
+
+    return (
+      <div>
+        <ControlLabel>Enter completed tasks below:</ControlLabel>
+        <Button bsStyle="primary" bsSize="xsmall" onClick={this.handleClick}>
+          Add task
+        </Button>
+        {this.state.enter.map(comp => comp)}
+      </div>
+    );
+  }
+}
