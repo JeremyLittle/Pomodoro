fa7032dda37a2674d3800fffba8016c3ae1f758f
evanlesmez <evan.lesmez@gmail.com>
2018-06-07 15:57:38 -0400

    button for leaderboard

--- pomodoro/src/Timer.js
+++ pomodoro/src/Timer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import EnterTasks from "./EnterTasks";
 import Stopwatch from "./Stopwatch";
+import { Button } from 'antd';
 
 export default class Timer extends Component {
   render() {
