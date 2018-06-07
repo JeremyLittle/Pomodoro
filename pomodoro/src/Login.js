7ea2027d952b93f0ca0f411a335cc23380af0f10
Marina Kun <kunmarina@gmail.com>
2018-06-07 13:20:46 -0400

    added more styling for login and register page

--- pomodoro/src/Login.js
+++ pomodoro/src/Login.js
@@ -17,33 +17,37 @@ export default class Login extends Component {
     };
   }
 
-  componentDidMount(){
-    firebase.auth().onAuthStateChanged((user) => {
-        console.log(user)
-        if (user!==null) {
-          this.setState({
-            user: true
-          })
-        } else {
-          user: false
-        }
-      });
-  }
+    updateField = (field, value) => {
+        this.setState({
+        [field]: value
+        });
+    };
 
-  updateField = (field, value) => {
-    this.setState({
-      [field]: value
-    });
-  };
+    signOut=()=>{
+        console.log(this.state)
+        firebase.auth().signInWithEmailAndPassword(this.state.username, this.state.password)
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log(user)
+            if (user!==null) {
+            this.setState({
+                user: true
+            })
+            } else {
+                user: false
+            }
+        });
+    }
 
     render(){
+        console.log(this.state)
         if(this.state.user){
-            return <Redirect to = '/entertasks'/>
+            return <Redirect to = '/timer'/>
         }
         return(
             <div className = "loginform">
                 <img src={tomato} className = "tomato"/>
                 <div className = "loginbox">
+                    <h1 id = "logintitle"> Log In </h1>
                     Email:  
                     <input
                         name = "username"
@@ -52,7 +56,6 @@ export default class Login extends Component {
                         onChange = {e=>this.updateField("username",e.target.value)}
                     />
                     <br></br>
-                    <br></br>
                     Password: 
                     <input
                         name = "password"
@@ -60,22 +63,17 @@ export default class Login extends Component {
                         value = {this.state.password}
                         onChange = {e=>this.updateField("password",e.target.value)}
                     />
-                    <ButtonToolbar>
-                        <button onClick={e=>this.handleClick(e)}>
+                    <br></br>
+                        <button id = "loginbut" onClick={this.signOut}>
                             {" "}
                             Log In {" "}
                         </button>
-                        <Link to = "./Register">
-                        <button>
-                            {" "}
-                            Register {" "}
-                        </button> 
-                        </Link>
-                        <button onClick = {logout}>
-                            {" "}
-                            Log out {" "}
-                        </button>
-                    </ButtonToolbar>
+                </div>
+                <div className = "register">
+                    <p id = "registerlink">Don't have an account?  </p>
+                    <Link to = "./Register">
+                    <p id = "registerlink2"> Register </p>
+                    </Link>
                 </div>
             </div>
                 
--- pomodoro/src/RouteC.js
+++ pomodoro/src/RouteC.js
@@ -6,6 +6,7 @@ import Register from "./Register.js";
 import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 import EnterTasks from "./EnterTasks.js";
 import firebase from './Firebase.js';
+import Timer from './Timer.js';
 
 export default class RouteC extends Component {
   render() {
@@ -13,7 +14,7 @@ export default class RouteC extends Component {
       <BrowserRouter>
         <div>
           <Redirect to="/login" />
-          <Route path="/entertasks" component={EnterTasks} />
+          <Route path="/timer" component={Timer} />
           <Route path="/app" component={App} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
