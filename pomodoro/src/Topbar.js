1c3cb472b8b8c96e2d43f75174144106e099b140
Noah French <noahjfrench@gmail.com>
2018-06-07 15:48:42 -0400

    huzzah huzzah

--- pomodoro/src/Topbar.js
+++ pomodoro/src/Topbar.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import "antd/dist/antd.css";
+import { Menu } from "antd";
+const MenuItemGroup = Menu.ItemGroup;
 
+//#d16429
 export default class Topbar extends Component {
   render() {
     return (
-      <Navbar fixedTop="true" inverse="true">
-        Hello There!
-      </Navbar>
+      <div>
+        <Menu mode="horizontal">
+          <Menu.Item key="timer">Timer</Menu.Item>
+          <Menu.Item key="profile">Profile</Menu.Item>
+          <Menu.Item key="leaderboard">Leaderboard</Menu.Item>
+        </Menu>
+      </div>
     );
   }
 }
