16cd5c2136c35a3fbb597ac683b7c5fd02eec982
Noah French <noahjfrench@gmail.com>
2018-06-07 12:01:38 -0400

    idk

--- pomodoro/src/firebase.js
+++ pomodoro/src/firebase.js
@@ -1,16 +1,16 @@
-import * as firebase from "firebase";
-import "firebase/auth";
+import * as firebase from 'firebase';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDrsrZyL1WB9dPyuxGWF8toSqHvnXK1hQE",
-  authDomain: "pomodoro-18245.firebaseapp.com",
-  databaseURL: "https://pomodoro-18245.firebaseio.com",
-  projectId: "pomodoro-18245",
-  storageBucket: "",
-  messagingSenderId: "83912180076"
-};
-firebase.initializeApp(config);
+    apiKey: 'AIzaSyDrsrZyL1WB9dPyuxGWF8toSqHvnXK1hQE',
+    authDomain: 'pomodoro-18245.firebaseapp.com',
+    databaseURL: 'https://pomodoro-18245.firebaseio.com',
+    projectId: 'pomodoro-18245',
+    storageBucket: '',
+    messagingSenderId: '83912180076'
+  };
+  firebase.initializeApp(config);
 
-export const ref = firebase.database().ref();
-export const firebaseAuth = firebase.auth;
-export default firebase;
+  export const ref = firebase.database().ref();
+  export const firebaseAuth = firebase.auth;
+
