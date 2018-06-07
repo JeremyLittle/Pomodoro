2a899af39fbbccdeec6482a00d67b524596d97ff
evanlesmez <evan.lesmez@gmail.com>
2018-06-07 09:44:52 -0400

    Deleted copies of files that others have (Just pushing timer)

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Firebase from './Firebase.js';
 import Stopwatch from './Stopwatch.js';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <Stopwatch/>
-        {/* <Firebase /> */}
       </div>
     );
   }
