b0555dad46fea21fc2064cd59a9bf654f72f40dc
Noah French <noahjfrench@gmail.com>
2018-06-07 16:15:26 -0400

    toolbar and colors and things!

--- pomodoro/src/Topbar.js
+++ pomodoro/src/Topbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "antd/dist/antd.css";
 import { Menu } from "antd";
+import "./Topbar.css";
 const MenuItemGroup = Menu.ItemGroup;
 
 //#d16429
@@ -10,8 +11,12 @@ export default class Topbar extends Component {
     return (
       <div>
         <Menu mode="horizontal">
-          <Menu.Item key="timer">Timer</Menu.Item>
-          <Menu.Item key="profile">Profile</Menu.Item>
+          <Menu.Item key="timer">
+            <Link to="/timer">Timer</Link>
+          </Menu.Item>
+          <Menu.Item key="profile">
+            <Link to="/profile">Profile</Link>
+          </Menu.Item>
           <Menu.Item key="leaderboard">Leaderboard</Menu.Item>
         </Menu>
       </div>
