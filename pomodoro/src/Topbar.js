43f1362ff80d4b81b062a0cf2284dd6c4dd9e162
Sonali Luthar <sonaliluthar@d-172-25-79-243.dhcp.virginia.edu>
2018-06-08 10:57:28 -0400

    add logout button and currently loggin in

--- pomodoro/src/Topbar.js
+++ pomodoro/src/Topbar.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "antd/dist/antd.css";
-import { Menu } from "antd";
+import { Menu, Button } from "antd";
 import "./Topbar.css";
+import firebase from "./Firebase";
+
 const MenuItemGroup = Menu.ItemGroup;
 
-//#d16429
 export default class Topbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  componentDidMount() {
+    const usersRef = firebase
+      .database()
+      .ref(firebase.auth().currentUser.uid + "/name");
+    usersRef.on("value", snapshot => {
+      let name = snapshot.val();
+      this.setState({
+        name: name
+      });
+    });
+  }
+
   render() {
     return (
       <div>
@@ -20,6 +40,13 @@ export default class Topbar extends Component {
           <Menu.Item key="leaderboard">
             <Link to="/leaderboard">Leaderboard</Link>
           </Menu.Item>
+          <div>
+            {" "}
+            Currently logged into {this.state.name}
+            <Button key="logout" type="danger" className="logout">
+              <Link to="/logout">Logout</Link>
+            </Button>
+          </div>
         </Menu>
       </div>
     );
