96f6a21aa6812645b7a5a9ce424d8cb530060fac
Sung Joon Park <andrewpark1136@hotmail.com>
2018-06-08 09:16:27 -0400

    icon + browsername + variable

--- pomodoro/src/Register.js
+++ pomodoro/src/Register.js
@@ -24,14 +24,17 @@ export default class Register extends Component {
     });
   };
 
-  emptyInputs=()=>{
-      if(this.state.name === "" || this.state.age === "" || this.state.gender === ""){
-          return true; 
-      }
-      else{
-          return false; 
-      }
-  }
+  emptyInputs = () => {
+    if (
+      this.state.name === "" ||
+      this.state.age === "" ||
+      this.state.gender === ""
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   handleClick = e => {
     auth(this.state.username, this.state.password)
       .then(user => {
@@ -47,20 +50,22 @@ export default class Register extends Component {
             age: this.state.age,
             gender: this.state.gender,
             name: this.state.name,
-            password: this.state.password
+            password: this.state.password,
+            rank: "",
+            color: "",
+            picture: ""
           };
           let newPostKey = firebase
             .database()
             .ref(userID)
             .set(obj);
           this.setState({
-              username: "",
-              age: "",
-              name: "",
-              password: ""
-            
-          })
-          alert("Registration successful. Please login.")
+            username: "",
+            age: "",
+            name: "",
+            password: ""
+          });
+          alert("Registration successful. Please login.");
           logout();
         }
       })
@@ -91,42 +96,51 @@ export default class Register extends Component {
             value={this.state.password}
             onChange={e => this.updateField("password", e.target.value)}
           />
-          <br/>
+          <br />
           Name
           <input
-            name = "name"
-            id = "input"
-            value = {this.state.name}
+            name="name"
+            id="input"
+            value={this.state.name}
             onChange={e => this.updateField("name", e.target.value)}
           />
-          <br/>
+          <br />
           Age
           <input
-            type = "number"
-            name = "age"
-            id = "input"
-            value = {this.state.age}
-            onChange={!isNaN(this.state.age)&&(e => this.updateField("age", e.target.value))}
+            type="number"
+            name="age"
+            id="input"
+            value={this.state.age}
+            onChange={
+              !isNaN(this.state.age) &&
+              (e => this.updateField("age", e.target.value))
+            }
           />
           <br />
           Gender
-          <select id = "input" onChange = {e=>this.updateField("gender", e.target.value)}>
-              <option value = "Female">Female</option>
-              <option value = "Male">Male</option>
-              <option value = "Other">Other</option>
+          <select
+            id="input"
+            onChange={e => this.updateField("gender", e.target.value)}
+          >
+            <option value="Female">Female</option>
+            <option value="Male">Male</option>
+            <option value="Other">Other</option>
           </select>
-          <br/>
-          <button id="registerbut" disabled = {this.emptyInputs()}
-          onClick={(!this.emptyInputs())&&(e => this.handleClick(e))}>
+          <br />
+          <button
+            id="registerbut"
+            disabled={this.emptyInputs()}
+            onClick={!this.emptyInputs() && (e => this.handleClick(e))}
+          >
             {" "}
             Register{" "}
           </button>
         </div>
-        <div className = "register">
-            <p id = "registerlink">Already registered?  </p>
-            <Link to = "./Login">
-            <p id = "registerlink2"> Login </p>
-            </Link>
+        <div className="register">
+          <p id="registerlink">Already registered? </p>
+          <Link to="./Login">
+            <p id="registerlink2"> Login </p>
+          </Link>
         </div>
       </div>
     );
