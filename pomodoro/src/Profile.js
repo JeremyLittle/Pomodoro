ef3eff9c9a0e8eb82663147d49778464b3c301ef
Sonali Luthar <sonaliluthar@d-172-25-79-243.dhcp.virginia.edu>
2018-06-08 12:03:27 -0400

    edit feature on profile page

--- pomodoro/src/Profile.js
+++ pomodoro/src/Profile.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import ImageDrop from "./ImageDrop";
-
 import request from "superagent";
 import InfoForm from "./InfoForm";
-import profilestyle from "./profile.css";
+import "./profile.css";
 import Topbar from "./Topbar.js";
+import {
+  Checkbox,
+  Radio,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  HelpBlock,
+  Button,
+  Alert
+} from "react-bootstrap";
+import firebase from "./Firebase.js";
 
 const CLOUDINARY_UPLOAD_PRESET = "bmzjbxoq";
 const CLOUDINARY_UPLOAD_URL =
@@ -15,9 +25,48 @@ class Profile extends Component {
     uploadedFile: null,
     uploadedFileCloudinaryUrl: "",
     showForm: false,
-    birthday: "",
-    showImageDrop: false
+    age: "",
+    gender: "",
+    name: "",
+    showImageDrop: false,
+    ageEdit: false,
+    genEdit: false,
+    editor: ""
   };
+
+  componentDidMount() {
+    const nameRef = firebase
+      .database()
+      .ref(firebase.auth().currentUser.uid + "/name");
+
+    const genderRef = firebase
+      .database()
+      .ref(firebase.auth().currentUser.uid + "/gender");
+
+    const ageRef = firebase
+      .database()
+      .ref(firebase.auth().currentUser.uid + "/age");
+
+    nameRef.on("value", snapshot => {
+      let name = snapshot.val();
+      this.setState({
+        name: name
+      });
+    });
+
+    genderRef.on("value", snapshot => {
+      let gender = snapshot.val();
+      this.setState({
+        gender: gender
+      });
+    });
+    ageRef.on("value", snapshot => {
+      let age = snapshot.val();
+      this.setState({
+        age: age
+      });
+    });
+  }
 
   handleImageUpload = file => {
     let upload = request
@@ -42,26 +91,15 @@ class Profile extends Component {
     this.setState({
       uploadedFile: files[0]
     });
-
     this.handleImageUpload(files[0]);
   };
 
   handleEdit = () => {
-    console.log("Heere");
-    console.log(String(this.state.birthday));
     this.setState({
       showForm: true
     });
   };
 
-  handleSubmit = e => {
-    console.log("Here");
-    this.setState({
-      birthday: e.target.value
-    });
-    console.log("Eliza");
-    console.log(String(this.state.birthday));
-  };
   handleImageEdit = () => {
     this.setState({
       showImageDrop: true
@@ -74,27 +112,97 @@ class Profile extends Component {
     });
   };
 
+  editAge = () => {
+    this.setState({
+      ageEdit: true,
+      editor: (
+        <FormGroup controlId="formBasicText">
+          <FormControl
+            type="text"
+            placeholder="New age"
+            // value={this.state.birthday}
+            onChange={this.changeEdit}
+          />
+          <FormControl.Feedback />
+          <Button bsStyle="primary" bsSize="xsmall" onClick={this.submitEdit}>
+            submit
+          </Button>
+        </FormGroup>
+      )
+    });
+  };
+
+  editGender = () => {
+    this.setState({
+      genEdit: true,
+      editor: (
+        <FormGroup controlId="formBasicText">
+          <FormControl
+            type="text"
+            placeholder="New gender"
+            onChange={this.changeEdit}
+          />
+          <FormControl.Feedback />
+          <Button bsStyle="primary" bsSize="xsmall" onClick={this.submitEdit}>
+            submit
+          </Button>
+        </FormGroup>
+      )
+    });
+  };
+
+  submitEdit = e => {
+    e.preventDefault();
+    let newChange = null;
+    let path = null;
+    if (this.state.ageEdit) {
+      newChange = this.state.age;
+      path = "age";
+    } else if (this.state.genEdit) {
+      newChange = this.state.gender;
+      path = "gender";
+    }
+    if (newChange !== "") {
+      let taskRef = firebase
+        .database()
+        .ref(firebase.auth().currentUser.uid)
+        .child(path)
+        .set(newChange);
+    }
+    this.setState({
+      age: "",
+      gender: "",
+      editor: "",
+      ageEdit: false,
+      genEdit: false
+    });
+  };
+
+  changeEdit = e => {
+    if (this.state.ageEdit) {
+      this.setState({ age: e.target.value });
+    } else if (this.state.genEdit) {
+      this.setState({ gender: e.target.value });
+    }
+  };
+
   render() {
     const style = {
       height: "200px",
       width: "200px"
     };
     const tinyFont = {
-      fontSize: "12px"
+      fontSize: "12px",
+      color: "blue"
     };
 
-    let form = null;
-    console.log(this.state);
-    if (this.state.showForm) {
-      form = <InfoForm onSubmit={e => this.handleSubmit(e)} />;
-    }
     return (
-      <div>
+      <div id="general">
         <Topbar />
         <br />
         <div>
           {" "}
-          <h1>User Name</h1>{" "}
+          <h1>{this.state.name}</h1>{" "}
           <h20 style={tinyFont} onClick={this.handleImageEdit}>
             Edit
           </h20>
@@ -131,16 +239,22 @@ class Profile extends Component {
           <div>
             {" "}
             <h2> About </h2>
-            <h4 onClick={this.handleEdit}>Edit {form}</h4>
           </div>
-          {this.state.birthday === "" ? null : (
-            <h3>Birthday: {String(this.state.birthday)} </h3>
-          )}
-          {/* <h3>Birthday:</h3>
-                    <h3> Gender: </h3> */}
+          <h3>Age: {this.state.age} </h3>
+          <h20 style={tinyFont} onClick={this.editAge}>
+            {" "}
+            Edit{" "}
+          </h20>
+          <h3> Gender: {this.state.gender}</h3>
+          <h20 style={tinyFont} onClick={this.editGender}>
+            {" "}
+            Edit{" "}
+          </h20>
+          {this.state.editor}
         </div>
       </div>
     );
   }
 }
+
 export default Profile;
